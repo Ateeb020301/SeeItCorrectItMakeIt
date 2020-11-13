1f095b4147b03e9d1422fea0af4f733223eef5f4
Ateeb020301 <ateeb@live.no>
2020-11-13 07:15:21 +0100

    hi

--- assets/script/animation.js
+++ assets/script/animation.js
@@ -1,5 +1,18 @@
 var sloganArr = ["QUBIT","PIXELS","FIREWALL","YOUR BRAIN?"]
 var slogan = document.getElementById("slogan");
+var buisnessUl = document.getElementById("buisnessUl");
+var creationUl = document.getElementById("creationUl");
+var cyberUl = document.getElementById("cyberUl");
+/*
+buisnessUl.onclick = function(){
+   if(buisnessUl.style.marginBottom=="0px"){
+      buisnessUl.style.marginBottom = "300px";
+   }else if(buisnessUl.style.marginBottom== "300px"){
+      buisnessUl.style.marginBottom = "0px";
+   }
+}*/
+
+
 
 function endlessLoop(){
    setInterval(function(){
